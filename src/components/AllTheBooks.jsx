31b0be6d8afd939237b33dab1a5037c6342ac19a
MattiaSusin <m.susin28@gmail.com>
2024-06-26 17:03:54 +0200

    Sistemazione

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,42 +1,37 @@
-import { Component } from "react";
-import {Card, CardBody, CardImg, CardText, CardTitle, Col, Container, Row } from "react-bootstrap";
-import fantasy from '../assets/fantasy.json'
-class AllTheBooks extends Component {
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Row  from "react-bootstrap/Row";
+import fantasy from "../assets/fantasy.json"
+import CardBody  from "react-bootstrap/CardBody";
+import CardTitle  from "react-bootstrap/CardTitle";
+import CardText  from "react-bootstrap/CardText";
+import CardImg  from "react-bootstrap/CardImg";
+import { Col } from "react-bootstrap";
 
-state={
-    book:fantasy[0]
-}
 
-render(){
-    return(
-        <Container>
-            <Row>
-                {
-                    fantasy.map(fanta=> {
-                        return(
-                            <Col  key={`fanta-${fanta.asin}`}>
-                               {
-                                <Card>
-                                    <CardImg src={this.state.img}></CardImg>
-                                    <CardBody>
-                                        <CardTitle>{fantasy.title}</CardTitle>
-                                        <CardText>{fantasy.category}</CardText>
-                                        <CardText>{fantasy.price}</CardText>
-                                        <CardText></CardText>
-                                    </CardBody>
-                                </Card>
-                               }
-                            </Col>
-                        )
-                    }
 
-                    )
-                }
-            </Row>
-        </Container>
-    )
-}
 
-}
+const AllTheBooks = props => (
+    <Row className="justify-content-center">
+        {fantasy.map((fantasy)=>{
+            return(
+            <Col className="CardBook" lg={3}>
+                <Card className=" CardTot border-0">
+                <CardImg className="ImgCard" src={fantasy.img}></CardImg>
+                <CardBody className=" CardBodySiz">
+                    <CardTitle className="Titlecard">{fantasy.title}</CardTitle>
+                    <CardText>{fantasy.category}</CardText>
+                    <CardText></CardText>
+                </CardBody>
+                <div className="row justify-content-center">
+                    <CardText className="CardPrice col-3 d-flex justify-content-start border border-black bg-dark text-light rounded-pill" >{fantasy.price}€</CardText>
+                    <button className=" btnView col-6 border rounded-pill ms-5 border-0">View</button>
+                </div>
+                </Card>
+            </Col>
+            )
+        })}
+    </Row>
+);
 
 export default AllTheBooks;
